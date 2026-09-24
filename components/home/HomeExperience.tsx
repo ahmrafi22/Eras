80f1f010b497b5c2b6178c32a265ba129d31d423
ahmrafi22 <ahmrafi22work@gmail.com>
2026-09-24 20:44:32 +0600

    feat: add Webflow and Lenis runtime bridge

---
+++ components/home/HomeExperience.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import Script from "next/script";
+import { useEraMotion } from "@/lib/era/motion/useEraMotion";
+
+declare global {
+  interface Window {
+    Webflow?: {
+      init: () => void;
+      ready: () => void;
+    };
+  }
+}
+
+const jqueryUrl =
+  "https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=6a068da7ad91b057365bf967";
+const webflowUrl =
+  "https://cdn.prod.website-files.com/6a068da7ad91b057365bf967/js/webflow.a0aa6ca1.b7683852b8a60d8e.js";
+
+export function HomeExperience() {
+  useEraMotion();
+
+  const initializeWebflow = () => {
+    window.Webflow?.init();
+    window.Webflow?.ready();
+  };
+
+  return (
+    <>
+      <Script id="era-jquery" src={jqueryUrl} strategy="afterInteractive" />
+      <Script
+        id="era-webflow"
+        src={webflowUrl}
+        strategy="afterInteractive"
+        onLoad={initializeWebflow}
+      />
+    </>
+  );
+}
